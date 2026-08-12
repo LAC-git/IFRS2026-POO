7d45791073b8c2a88cd2c818dcdae7addce95124
Lilith <98246849+LAC-git@users.noreply.github.com>
2026-08-12 13:34:17 -0300

    Iniciando exercício 3, métowqdo estudantesAprovados

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+
+import Estudante;
+
+public class Main {
+    public static void main(String[] args) {
+    }
+
+    public estudantesAprovados(Estudantes[] ests) {
+        for (Estudante e: ests) {
+        }
+    }
+}
