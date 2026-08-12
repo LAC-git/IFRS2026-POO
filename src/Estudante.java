dba0066033608e1faf0f24ca29680eb010a936d4
Lilith <98246849+LAC-git@users.noreply.github.com>
2026-08-12 13:06:02 -0300

    Comentario adicionado ao construtor da classe Estudante

--- src/Estudante.java
+++ src/Estudante.java
@@ -6,6 +6,7 @@ public class Estudante {
     public Estudante(String nome) {
         this.nome = nome;
         notas = new double[5];
+        // O array de notas é inicializado assim: [0.0, 0.0, 0.0, 0.0, 0.0]
     }
 }
 
