96b8e416938a4c52afc924b2443710d3c99a0c9b
Lilith <98246849+LAC-git@users.noreply.github.com>
2026-08-12 13:03:22 -0300

    Criado construtor pra classe Estudante

--- src/Estudante.java
+++ src/Estudante.java
@@ -2,5 +2,10 @@
 public class Estudante {
     private String nome;
     private double[] notas;
+
+    public Estudante(String nome) {
+        this.nome = nome;
+        notas = new double[5];
+    }
 }
 
