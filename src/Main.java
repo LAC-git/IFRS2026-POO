b0133eafd709bc7cf3bd3bd8015da084f9e1977e
LAC-git <98246849+LAC-git@users.noreply.github.com>
2026-08-13 09:28:19 -0300

    Implementei o método estudantesAprovados

--- src/Main.java
+++ src/Main.java
@@ -6,8 +6,14 @@ public class Main {
     public static void main(String[] args) {
     }
 
-    public estudantesAprovados(Estudantes[] ests) {
+    public ArrayList<Estudante> estudantesAprovados(Estudante[] ests) {
+        ArrayList<Estudante> aprovados = new ArrayList<>();
+
         for (Estudante e: ests) {
+            if (e.calculaMedia() >= 6.0) { aprovados.add(e); }
         }
+
+        if (aprovados.size() == 0) { return null; }
+        return aprovados;
     }
 }
