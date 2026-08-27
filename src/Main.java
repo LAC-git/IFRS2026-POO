025f885e1adfb89558f3be7af60f887055f4127d
Lilith <98246849+LAC-git@users.noreply.github.com>
2026-08-27 14:05:43 -0300

    Removi Estudante pq não precisa pro exercicio 5. Limpei a Main pra iniciar a ativiade

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,4 @@ import java.util.ArrayList;
 public class Main {
     public static void main(String[] args) {
     }
-
-    public ArrayList<Estudante> estudantesAprovados(Estudante[] ests) {
-        ArrayList<Estudante> aprovados = new ArrayList<>();
-
-        for (Estudante e: ests) {
-            if (e.calculaMedia() >= 6.0) { aprovados.add(e); }
-        }
-
-        if (aprovados.size() == 0) { return null; }
-        return aprovados;
-    }
 }
