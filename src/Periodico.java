996d6a9a3ab89508860418b50fd0be38f40b0c77
Lilith <98246849+LAC-git@users.noreply.github.com>
2026-08-27 14:38:10 -0300

    ACHO que terminei de implementar o Periodico

--- src/Periodico.java
+++ src/Periodico.java
@@ -1,3 +1,19 @@
 
-public class Periodico {
+public class Periodico extends Texto {
+    private int numeroDoVolume;
+
+    public Periodico(String titulo, int numeroDoVolume) {
+        super(titulo);
+
+        if (numeroDoVolume < 0) {
+            // Não pode ser menor que zero, mas também vou usar essa excecção aí pq ela é unchecked e n quero ter q lidar com ela
+            throw new IllegalArgumentException();
+        }
+
+        this.numeroDoVolume = numeroDoVolume;
+    }
+
+    public int getNumeroDoVolume() {
+        return numeroDoVolume;
+    }
 }
