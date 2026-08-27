6d6470bda73d505729943843c4dd9e4010c5d122
Lilith <98246849+LAC-git@users.noreply.github.com>
2026-08-27 14:48:50 -0300

    Implementar funções de emprestar e devolver no Livro

--- src/Livro.java
+++ src/Livro.java
@@ -16,4 +16,25 @@ public class Livro extends Texto {
         // Bora usar esse toString grátis do Enums
         return situacao.toString();
     }
+
+    // Representam as operações de emprestar e devolver,
+    // num projeto de verdade provavelmente teria que ter mais código pra guardar quem emprestou etc
+
+    public boolean emprestar() {
+        if (situacao == Situacao.DISPONIVEL) {
+            this.situacao = Situacao.EMPRESTADO;
+            return true; // True = deu certo
+        }
+
+        // daria pra usar um 'throw' e exceções mas to sem tempo
+        return false; // False = já foi emprestado antes
+    }
+
+    public boolean devolver() {
+        if (situacao == Situacao.EMPRESTADO) {
+            this.situacao = Situacao.DISPONIVEL;
+            return true;
+        }
+        return false;
+    }
 }
